044d10151d1e8e2ba605dd8f9c118e280cd6800a
Dr Nic Williams <drnicwilliams@gmail.com>
2016-02-16 16:44:34 +1000

    try bumping ruby to 2.2.4

--- ci/tasks/build-docs-app.sh
+++ ci/tasks/build-docs-app.sh
@@ -5,9 +5,14 @@ set -e # fail fast
 cd docs
 
 cd docs-book
-bundle
+sed -i "s/ruby '2.0.0'/ruby '2.2.4'/g" Gemfile
+bundle update
+
 bin/bookbinder bind local
 cd final_app
+
+sed -i "s/ruby '2.0.0'/ruby '2.2.4'/g" Gemfile
+bundle update
 
 cat >manifest.yml <<EOF
 ---
