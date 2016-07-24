40e5f1a4d78c7b5d7761c087b3cd68a8d44aa228
Dr Nic Williams <drnicwilliams@gmail.com>
2016-07-25 09:31:36 +1000

    ruby req changed; but still don't care really

--- ci/tasks/build-docs-app.sh
+++ ci/tasks/build-docs-app.sh
@@ -6,7 +6,7 @@ set -x # show commands
 git clone docs docs-app
 
 cd docs-app/docs-book
-sed -i "s/ruby '2.0.0'//g" Gemfile
+sed -i "s/^ruby.*//g" Gemfile
 cat Gemfile
 bundle update --local
 bundle install --binstubs
@@ -14,7 +14,7 @@ bundle install --binstubs
 ./bin/bookbinder bind local --verbose
 cd final_app
 
-sed -i "s/ruby '2.0.0'//g" Gemfile
+sed -i "s/^ruby.*//g" Gemfile
 echo "gem 'puma'" >> Gemfile
 bundle update --local
 bundle install --binstubs
